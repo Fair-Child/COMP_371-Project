48773a405ad691e1fcc157cfac01d916792de228
msalaciak <matt.salaciak@gmail.com>
2020-04-05 17:25:39 -0400

    fixed textures

--- Source/texture-loader.h
+++ Source/texture-loader.h
@@ -39,8 +39,8 @@ GLuint loadTexture(const char *filename)
    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
 
 
 
@@ -63,6 +63,8 @@ GLuint loadTexture(const char *filename)
                               GL_RGBA, GL_UNSIGNED_BYTE, data);
              }
              glGenerateMipmap(GL_TEXTURE_2D);
+    
+
 
 
   // Step5 Free resources
